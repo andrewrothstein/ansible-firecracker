511ea2c3d2e6bd56dc5dd0a62d6b592ba80a3b2a
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-01-02 22:23:09 -0500

    clean up for recent versions

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -e
 DIR=~/Downloads
 MIRROR=https://github.com/firecracker-microvm/firecracker/releases/download
 
@@ -7,30 +8,40 @@ dl()
     local app=$1
     local ver=$2
     local arch=$3
-    local file="${app}-${ver}-${arch}"
+    local file="${app}-${ver}-${arch}.tgz"
     local url=$MIRROR/$ver/$file
     local lfile=$DIR/$file
 
     if [ ! -e $lfile ];
     then
-        wget -q -O $lfile $url
+        curl -sSL -f -o $lfile $url
     fi
 
-    printf "      # %s\n" $url
-    printf "      %s: sha256:%s\n" $arch $(sha256sum $lfile | awk '{print $1}')
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $arch $(sha256sum $lfile | awk '{print $1}')
 }
 
 dlver() {
     local ver=$1
-    printf "  jailer:\n"
-    printf "    %s:\n" $ver
-    dl jailer $ver aarch64
-    dl jailer $ver x86_64
-    printf "  firecracker:\n"
-    printf "    %s:\n" $ver
+    printf "  %s:\n" $ver
     dl firecracker $ver aarch64
     dl firecracker $ver x86_64
 }
 
 printf "firecracker_checksums:\n"
-dlver ${1:-v0.23.1}
+dlver v0.22.3
+dlver v0.22.4
+dlver v0.23.2
+dlver v0.23.3
+dlver v0.23.4
+dlver v0.23.5
+dlver v0.24.0
+dlver v0.24.1
+dlver v0.24.2
+dlver v0.24.3
+dlver v0.24.4
+dlver v0.24.5
+dlver v0.24.6
+dlver v0.25.0
+dlver v0.25.1
+dlver ${1:-v0.25.2}
